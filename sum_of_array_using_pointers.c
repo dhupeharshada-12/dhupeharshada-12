e2da3b9997c9697bf2fbfd21ba80e1e81b066dec
Harshada Dhupe <dhupeharshada@gmail.com>
2026-06-15 21:20:04 +0530

    Add sum_of_array_using_pointers.c
    Implement a program to calculate the sum of an array using pointers.

---
+++ sum_of_array_using_pointers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[5], i, sum = 0;
+    int *ptr;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    ptr = arr;
+
+    for(i = 0; i < 5; i++)
+    {
+        sum = sum + *(ptr + i);
+    }
+
+    printf("Sum of array elements = %d", sum);
+
+    return 0;
+}
