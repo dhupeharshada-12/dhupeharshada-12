fafa12a70b9ef7d6f97c1ba6762b0d70457e19f4
Harshada Dhupe <dhupeharshada@gmail.com>
2026-06-21 21:07:17 +0530

    Add string reversal using pointers in C
    This C program reverses a string using pointers. It uses two pointer variables to access the beginning and end of the string and swaps characters until the string is reversed. The project demonstrates pointer manipulation, string handling, and character operations in C programming, making it a good intermediate-level GitHub project

---
+++ string_reverse_using_pointer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    char *start, *end, temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    start = str;
+    end = str + strlen(str) - 1;
+
+    while(start < end)
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed String: %s", str);
+
+    return 0;
+}
